f48bc95c5e68a419192d4acc678bdbe905654199
YoannRose <roseyoann@gmail.com>
2020-02-28 17:54:18 +0100

    TestPanier de Yoann

--- src/main/java/Autom4/Shopizer/MenuHorizontale.java
+++ src/main/java/Autom4/Shopizer/MenuHorizontale.java
@@ -7,5 +7,8 @@ public class MenuHorizontale {
 	
 	@FindBy (xpath="//div[contains(@class,'mainmenu-area')]//a[text()='Tables']")
 	WebElement optionTables ;
+	
+	@FindBy (xpath="//div[@class=\"mainmenu hidden-xs\"]/nav/ul/li/a[.=\"Bedroom\"]")
+	WebElement bedroomButton ;
 
 }
--- src/test/java/Autom4/Shopizer/PanierTest.java
+++ src/test/java/Autom4/Shopizer/PanierTest.java
@@ -1,5 +1,27 @@
 package Autom4.Shopizer;
 
-public class PanierTest {
+import java.util.concurrent.TimeUnit;
 
+import org.junit.Test;
+import org.junit.Before;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class PanierTest {
+	
+	WebDriver driver =  new ChromeDriver();
+	String url= "http://192.168.102.67:8080/shopizer";
+
+	@Before
+	public void setup() {
+		driver.get(url);
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);	
+	}
+	
+	@Test
+	public void testPanierShopizer() {
+	
+	}
+	
 }
