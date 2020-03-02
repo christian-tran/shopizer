2704879ae1fffe0e6d2c628ceb96e99b9b0ed8df
BrahimBenladghem <brahim.benladghem@gmail.com>
2020-03-02 10:06:39 +0100

    ajout VerificationMarchandisesTest

--- src/main/java/Autom4/Shopizer/PageAcceuil.java
+++ src/main/java/Autom4/Shopizer/PageAcceuil.java
@@ -1,7 +1,31 @@
 package Autom4.Shopizer;
 
-public class PageAcceuil extends MenuHorizontal{
-	
-	
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 
+public class PageAcceuil extends MenuHorizontal {
+	
+	@FindBy (id="150")
+	WebElement thaiFlatCussion;
+
+	@FindBy (id="50")
+	WebElement compactNightTable;
+	
+	@FindBy (id="100")
+	WebElement productWithOptions;
+	
+	@FindBy (id="154")
+	WebElement edgeConsole;
+	
+	@FindBy (id="153")
+	WebElement tableDecorationStorage;
+
+	@FindBy (id="151")
+	WebElement asianRosewoodConsole;
+	
+	@FindBy (id="152")
+	WebElement antiqueRecycledWoodStorage;
+	
+	@FindBy (id="400")
+	WebElement coffeeTableAccacia;
 }
---
+++ src/test/java/Autom4/Shopizer/VerfificationDesMarchandisesTest.java
@@ -0,0 +1,11 @@
+package Autom4.Shopizer;
+
+public class VerfificationDesMarchandisesTest {
+	
+	// 1 Ouvrir un navigateur et accéder à l'application Shopizer:
+
+	// 2 Verifier la présence des éléments suivant :
+	
+	// 3 Verifier le prix actuel de tous les éléments :
+	
+}
